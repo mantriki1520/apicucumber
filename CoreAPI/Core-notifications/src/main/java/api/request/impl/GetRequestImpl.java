10a5ddea42853ad88364c421f0988a99561e56d3
mantriki1520 <mantriki1520@gmail.com>
2025-03-12 23:56:44 +0530

    Create GetRequestImpl.java

---
+++ CoreAPI/Core-notifications/src/main/java/api/request/impl/GetRequestImpl.java
@@ -0,0 +1,25 @@
+package api.request.impl;
+
+import api.request.HttpRequestImpl;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequestImpl extends HttpRequestImpl {
+
+    @Override
+    public Response request() {
+        Response response;
+        try {
+            response = given()
+                    .header("auth", "")
+                    .contentType("application/json")
+                    .when()
+                    .get(getUrl());
+        } catch (Exception exception) {
+            throw exception;
+            //throw new RuntimeException("invalid request");
+        }
+        return response;
+    }
+}
