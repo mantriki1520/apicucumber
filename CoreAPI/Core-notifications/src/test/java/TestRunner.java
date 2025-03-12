42dd551c3b01540954e0728949e204763a9e96cc
mantriki1520 <mantriki1520@gmail.com>
2025-03-12 23:37:32 +0530

    Create TestRunner.java

---
+++ CoreAPI/Core-notifications/src/test/java/TestRunner.java
@@ -0,0 +1,17 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src\\test\\resources\\FeatureFiles\\"
+        , glue = {"org.stepdefinitions"},
+        tags = "@VCT-510 ",
+        plugin = {"pretty", "html:test-output", "json:target/cucumber/cucumber.json", "html:target/cucumber-html-report.html", "rerun:target/failedrerun.txt"}
+)
+
+
+public class TestRunner {
+
+
+}
